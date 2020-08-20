b82abdc9f0f3839e887f0b9c3fefed609382be0e
hconhegg <heggholmen3@gmail.com>
2020-08-20 12:57:38 +0200

    graying out sales page if you do not represent a company that sells something.

--- showroom-client/src/app/dashboard/dashboard.component.ts
+++ showroom-client/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Page, PAGES} from '../shared/page.constants';
 import {SnackbarService} from '../shared/snackbar.service';
+import {SALES_ROUTE} from '../shared/routing.constants';
+import {CompanyService} from '../shared/company.service';
+import {StoreService} from '../shared/store.service';
 
 
 @Component({
@@ -12,12 +15,25 @@ export class DashboardComponent implements OnInit {
 
   cards: Page[] = PAGES;
 
-  constructor(private snackBarService: SnackbarService) {
-
+  constructor(private snackBarService: SnackbarService,
+              private companyService: CompanyService,
+              private storeService: StoreService) {
   }
 
   ngOnInit(): void {
     this.snackBarService.showIntroPopup();
   }
+
+  getCards(): Page[] {
+    const salesPage = this.cards.find(card => card.route === SALES_ROUTE);
+    salesPage.active = this.isActingAsStore();
+    return this.cards;
+  }
+
+  private isActingAsStore(): boolean {
+    const actingCompanyId = this.companyService.getActingCompany().id;
+    return !!this.storeService.getStore(actingCompanyId);
+  }
+
 
 }
